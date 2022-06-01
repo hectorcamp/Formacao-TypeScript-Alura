d5a506021d710659835b01156a5429770331f791
hectorcamp <hectorcamp@gmail.com>
2022-06-01 00:06:17 -0300

    Benefícios da tipagem estática

---
+++ TypeScript_parte_1/dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,13 @@
+import { Negociacao } from "../models/negociacao.js";
+export class NegociacaoController {
+    constructor() {
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+    }
+    adiciona() {
+        const negociacao = new Negociacao(this.inputData.value, this.inputQuantidade.value, this.inputValor.value);
+        console.log(negociacao);
+    }
+    ;
+}
