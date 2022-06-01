7fe2959f116dd7827b57f0d29b8a5bb0e83ff3c0
hectorcamp <hectorcamp@gmail.com>
2022-06-01 00:32:36 -0300

    Benefícios da tipagem estática

--- TypeScript_parte_1/app/controllers/negociacao-controller.ts
+++ TypeScript_parte_1/app/controllers/negociacao-controller.ts
@@ -1,9 +1,9 @@
 import { Negociacao } from "../models/negociacao.js";
 
 export class NegociacaoController {
-    private inputData;
-    private inputQuantidade;
-    private inputValor;
+    private inputData: HTMLInputElement;
+    private inputQuantidade: HTMLInputElement;
+    private inputValor: HTMLInputElement;
 
     constructor() {
         this.inputData = document.querySelector('#data');
@@ -11,16 +11,17 @@ export class NegociacaoController {
         this.inputValor = document.querySelector('#valor');
     }
 
-    adiciona() {
-        const negociacao = new Negociacao(
-            this.inputData.value,
-            this.inputQuantidade.value,
-            this.inputValor.value,
-        );
-            
+    adiciona(): void {
+        const negociacao = this.criaNegociacao
         console.log(negociacao);
-    };
+    }
 
-    
+    criaNegociacao(): Negociacao {
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(date, quantidade, valor);
+    }
         
 }
--- TypeScript_parte_1/dist/js/controllers/negociacao-controller.js
+++ TypeScript_parte_1/dist/js/controllers/negociacao-controller.js
@@ -6,8 +6,14 @@ export class NegociacaoController {
         this.inputValor = document.querySelector('#valor');
     }
     adiciona() {
-        const negociacao = new Negociacao(this.inputData.value, this.inputQuantidade.value, this.inputValor.value);
+        const negociacao = this.criaNegociacao;
         console.log(negociacao);
     }
-    ;
+    criaNegociacao() {
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(date, quantidade, valor);
+    }
 }
