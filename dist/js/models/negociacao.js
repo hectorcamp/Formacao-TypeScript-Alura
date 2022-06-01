941e671d6060234d04e89649bba1612505ee0667
hectorcamp <hectorcamp@gmail.com>
2022-05-31 22:58:12 -0300

    TypeScript e o compilador

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,35 +1,19 @@
-var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
-    if (kind === "m") throw new TypeError("Private method is not writable");
-    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
-    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
-    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
-};
-var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
-    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
-    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
-    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
-};
-var _Negociacao_data, _Negociacao_quantidade, _Negociacao_valor;
 export class Negociacao {
     constructor(data, quantidade, valor) {
-        _Negociacao_data.set(this, void 0);
-        _Negociacao_quantidade.set(this, void 0);
-        _Negociacao_valor.set(this, void 0);
-        __classPrivateFieldSet(this, _Negociacao_data, data, "f");
-        __classPrivateFieldSet(this, _Negociacao_quantidade, quantidade, "f");
-        __classPrivateFieldSet(this, _Negociacao_valor, valor, "f");
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
     }
     get data() {
-        return __classPrivateFieldGet(this, _Negociacao_data, "f");
+        return this._data;
     }
     get quantidade() {
-        return __classPrivateFieldGet(this, _Negociacao_quantidade, "f");
+        return this._quantidade;
     }
     get valor() {
-        return __classPrivateFieldGet(this, _Negociacao_valor, "f");
+        return this._valor;
     }
     get volume() {
-        return __classPrivateFieldGet(this, _Negociacao_quantidade, "f") * __classPrivateFieldGet(this, _Negociacao_valor, "f");
+        return this._quantidade * this._valor;
     }
 }
-_Negociacao_data = new WeakMap(), _Negociacao_quantidade = new WeakMap(), _Negociacao_valor = new WeakMap();
