128972463fe20e2245ce1085c190a3c9f6c28724
hectorcamp <hectorcamp@gmail.com>
2022-06-03 17:49:40 -0300

    Integrando um view na página

---
+++ TypeScript_parte_2/app/views/negociacoes-view.ts
@@ -0,0 +1,27 @@
+export class NegociacoesView {
+
+    private elemento: HTMLElement;
+
+    constructor(seletor: string){
+        this.elemento = document.querySelector(seletor);
+    } 
+    template(): string {
+        return `
+        <table class="table table-hover table-borderes">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th> 
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+        `;
+    }
+
+    update(): void {
+        this.elemento.innerHTML = this.template();
+    }
+}
