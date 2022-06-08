d1507e6219b56d21381f5577e09fe648cc95efca
hectorcamp <hectorcamp@gmail.com>
2022-06-07 23:58:29 -0300

    Decorators

--- TypeScript_parte_3/app/src/decorators/inspect.ts
+++ TypeScript_parte_3/app/src/decorators/inspect.ts
@@ -1,5 +1,4 @@
-export function inspect() {
-    return function(
+    export function inspect(
         target: any,
         propertyKey: string,
         descriptor: PropertyDescriptor
@@ -7,11 +6,10 @@ export function inspect() {
         const metodoOriginal = descriptor.value;
         descriptor.value = function (...args: any []) {
             console.log(`--- Método ${propertyKey}`);
-            console.log(`------parâmetros: ${JSON.stringify(args)}`);
+            console.log(`------ parâmetros: ${JSON.stringify(args)}`);
             const retorno = metodoOriginal.apply(this, args);
             console.log(`------ retorno: ${JSON.stringify(retorno)}`);
             return retorno;
         }
         return descriptor;
     }
-}
