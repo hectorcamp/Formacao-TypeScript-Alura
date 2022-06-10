a203f27cf3b0ee6a3fbe09a31de7ca60f21999f1
hectorcamp <hectorcamp@gmail.com>
2022-06-10 01:38:05 -0300

    Interfaces

--- TypeScript_parte_3/app/src/models/negociacao.ts
+++ TypeScript_parte_3/app/src/models/negociacao.ts
@@ -3,22 +3,31 @@ export class Negociacao {
         private _data: Date, 
         public readonly quantidade: number, 
         public readonly valor: number
-    ) {}
+        ) {}
+        
+        public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
+            const exp = /-/g;
+            const date = new Date(dataString.replace(exp, ','));
+            const quantidade = parseInt(quantidadeString);
+            const valor = parseFloat(valorString);
+            return new Negociacao(date, quantidade, valor);
+        }
 
-    get volume(): number {
-        return this.quantidade * this.valor;
-    }
-
-    get data(): Date {
-        const data = new Date(this._data.getTime());
-        return data;
+        get volume(): number {
+            return this.quantidade * this.valor;
+        }
+        
+        get data(): Date {
+            const data = new Date(this._data.getTime());
+            return data;
+        }
+        
+        public paraTexto(): string {
+        return `
+        Data: ${this.data},
+        Quantidade: ${this.quantidade},
+        Valor: ${this.valor}
+    `;
     }
     
-    public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
-        const exp = /-/g;
-        const date = new Date(dataString.replace(exp, ','));
-        const quantidade = parseInt(quantidadeString);
-        const valor = parseFloat(valorString);
-        return new Negociacao(date, quantidade, valor);
-    }
 }
